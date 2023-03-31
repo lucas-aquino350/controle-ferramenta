4badda166f05a184ba2242322ff09c28dfec2346
Lucas Aquino <lucasaquino350@gmail.com>
2023-03-31 18:18:01 -0300

    inicia projeto

---
+++ src/main/java/br/com/suport/controleferramenta/ControleFerramentaApplication.java
@@ -0,0 +1,13 @@
+package br.com.suport.controleferramenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFerramentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFerramentaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/suport/controleferramenta/ControleFerramentaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.suport.controleferramenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFerramentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
