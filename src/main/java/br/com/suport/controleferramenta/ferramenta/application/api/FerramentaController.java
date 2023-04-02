049cbac8651364cbf8bf31273fbf8f6b830134a5
Lucas Aquino <lucasaquino350@gmail.com>
2023-04-02 15:12:01 -0300

    implementa controller

---
+++ src/main/java/br/com/suport/controleferramenta/ferramenta/application/api/FerramentaController.java
@@ -0,0 +1,22 @@
+package br.com.suport.controleferramenta.ferramenta.application.api;
+
+import br.com.suport.controleferramenta.ferramenta.application.service.FerramentaService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+public class FerramentaController implements FerramentaApi {
+
+    private final FerramentaService ferramentaSerrvice;
+
+    @Override
+    public FerramentaResponse postFerramenta(FerramentaRequest ferramentaRequest) {
+        log.info("[inicia] FerramentaController - portFerramenta");
+        FerramentaResponse ferramentaCriada = ferramentaSerrvice.criaFerramenta(ferramentaRequest);
+        log.info("[finaliza] FerramentaController - portFerramenta");
+        return ferramentaCriada;
+    }
+}
