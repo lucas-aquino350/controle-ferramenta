af1a49e6daef0a5cb73ed0cdd45d34db58dca828
Lucas Aquino <lucasaquino350@gmail.com>
2023-04-07 17:18:07 -0300

    implementar controller

--- src/main/java/br/com/suport/controleferramenta/colaborador/application/api/ColaboradorRequest.java
+++ src/main/java/br/com/suport/controleferramenta/colaborador/application/api/ColaboradorRequest.java
@@ -1,4 +1,22 @@
 package br.com.suport.controleferramenta.colaborador.application.api;
 
+import lombok.Value;
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.NotBlank;
+import java.util.UUID;
+
+@Value
 public class ColaboradorRequest {
+    
+    @NotBlank
+    private String nomeCompleto;
+    @CPF
+    @NotBlank
+    private String cpf;
+    @NotBlank
+    private String cargo;
+    @NotBlank
+    private String setor;
 }
