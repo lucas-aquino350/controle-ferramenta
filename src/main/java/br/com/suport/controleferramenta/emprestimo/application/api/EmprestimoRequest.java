6d46d14bbf2e1cddaa6e26a164f0ab6af4d87c53
Lucas Aquino <lucasaquino350@gmail.com>
2023-04-08 17:55:09 -0300

    implementa request e response

--- src/main/java/br/com/suport/controleferramenta/emprestimo/application/api/EmprestimoRequest.java
+++ src/main/java/br/com/suport/controleferramenta/emprestimo/application/api/EmprestimoRequest.java
@@ -1,4 +1,26 @@
 package br.com.suport.controleferramenta.emprestimo.application.api;
 
+import lombok.Value;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Value
 public class EmprestimoRequest {
+
+    @NotNull
+    private UUID idFerramenta;
+    @NotNull
+    private UUID idColaborador;
+    @NotBlank
+    private String motivo;
+    @NotBlank
+    private String ordemDeServico;
+    @NotNull
+    private LocalDate dataRetirada;
+    @NotNull
+    private LocalDate dataDevolucao;
+
 }
--- src/main/java/br/com/suport/controleferramenta/emprestimo/application/api/EmprestimoResponse.java
+++ src/main/java/br/com/suport/controleferramenta/emprestimo/application/api/EmprestimoResponse.java
@@ -1,4 +1,12 @@
 package br.com.suport.controleferramenta.emprestimo.application.api;
 
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
+@Builder
 public class EmprestimoResponse {
+    private UUID idEmprestimo;
 }
